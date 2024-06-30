5a0bc267afcf26443a6bba6cf6a0423343899122
David Zurita <108833714+DavidZuSan@users.noreply.github.com>
2024-06-30 20:28:36 +0200

    Comienzo proyecto Criptomenadas

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import CriptoSearchFrom from "./components/CriptoSearchFrom";
+
+function App() {
+  return (
+    <>
+      <div className="container">
+        <h1 className="app-title">
+          Cotizador de <span>Criptomonedas</span>
+        </h1>
+
+        <div className="content">
+          <CriptoSearchFrom />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
